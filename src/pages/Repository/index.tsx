5001dc963393319c930a4bc82a14ed586827bc67
José Augusto <jose.pasquali@snowmanlabs.com>
2021-08-14 22:17:49 -0300

    Repository style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './style';
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -10,7 +14,56 @@ const Repository: React.FC = () => {
   // utilizado para obter a url ou os parametros da url
   // aqui tipando a rota
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1> Repository: {params.repository} </h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://c.s-microsoft.com/pt-br/CMSImages/windows10-laptop.png?version=0bf90e10-f3b6-acaf-41fd-1c538d8c24bc"
+            alt="teste"
+          />
+          <div>
+            <strong>Title</strong>
+            <p>subtitle</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong> 11</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong> 11</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong> 11</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="aa">
+          <div>
+            <strong> aaa </strong>
+            <p> bbb</p>
+          </div>
+
+          {/* icone de seta */}
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
---
+++ src/pages/Repository/style.ts
@@ -0,0 +1,130 @@
+import styled, { css } from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+
+    &:hover {
+      color: #666;
+    }
+  }
+
+  svg {
+    margin-right: 4px;
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+`;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+
+    // responsavel por dizer que esse componente é um bloco/container
+    display: block;
+    // responsavel por tirar o estilo do link
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    // aqui aplica efeito quando usa o transform
+    transition: transform 0.2s;
+
+    // aqui outro exemplo utilizando o msm componente
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    // aqui fala se o proximo componente depois dele for igual a ele aplica o css
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      // se ajusta com o espaço que ela tem disponivel
+      flex: 1;
+      margin: 0px 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
